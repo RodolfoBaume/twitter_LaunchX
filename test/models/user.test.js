eb4c53bc7e400135ccf3a15d3267426f804c296c
RodolfoBaume <robalaz@hotmail.com>
2022-04-20 19:45:09 -0500

    Requerimiento 1

---
+++ test/models/user.test.js
@@ -0,0 +1,17 @@
+const User = require('./../../app/models/user')
+
+describe("Unit Test for User class",()=> {
+    test('Create an User object', () => {
+        //Aquí invocas el código que vas a usar en tu app
+        const user = new User(1, "RodolfoBaume", "Rodolfo", "Bio", "dateCreated", "lastUpdated")
+
+        // Aquí validas los resultados de ese código
+        // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("RodolfoBaume")
+        expect(user.name).toBe("Rodolfo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    });
+})
