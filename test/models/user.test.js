e8f7b8aca6c787c26f580607a464902e185a35dd
RodolfoBaume <robalaz@hotmail.com>
2022-04-20 19:49:19 -0500

    Requerimiento 2

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,7 +3,7 @@ const User = require('./../../app/models/user')
 describe("Unit Test for User class",()=> {
     test('Create an User object', () => {
         //Aquí invocas el código que vas a usar en tu app
-        const user = new User(1, "RodolfoBaume", "Rodolfo", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "RodolfoBaume", "Rodolfo", "Bio")
 
         // Aquí validas los resultados de ese código
         // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -11,7 +11,7 @@ describe("Unit Test for User class",()=> {
         expect(user.username).toBe("RodolfoBaume")
         expect(user.name).toBe("Rodolfo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined() // verifica que el valor no sea undefine
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
